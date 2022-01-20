6b778f78e142e8c4bd4f8e4f6b3fb7f49d2b1a6e
kortmiranda <kortneymiranda@outlook.com>
2022-01-20 13:33:29 -0600

    first test done

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-export default function Task({ task: {id, title, state}, onArchiveTask, onPinTask}) {
-    return (
-        <div className="list-item">
-            <input type="text" value={title} readOnly={true} />
-        </div>
-    )
+export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
+  return (
+    <div className="list-item">
+      <input type="text" value={title} readOnly={true} />
+    </div>
+  );
+}
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
 }
